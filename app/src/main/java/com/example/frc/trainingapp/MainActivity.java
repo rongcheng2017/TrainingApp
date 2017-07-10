a34c5f71cc1e07553cdb9010e7905f23c5a37dcd
fengrongcheng <fengrongcheng2017@outlook.com>
2017-07-10 18:13:54 +0800

    first commit

---
+++ app/src/main/java/com/example/frc/trainingapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.frc.trainingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "AppCompatActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Log.e(TAG, "sendMessage: " + "button is clicked");
+    }
+}
